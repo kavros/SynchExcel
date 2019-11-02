638631a0222ae690c2cc3d4f6ab96a5fcfca239e
kavros <akavroulakis@gmail.com>
2019-11-02 18:43:57 +0000

    adds sql queries and updates script

--- out/artifacts/SynchExcel_jar/runApp.bat
+++ out/artifacts/SynchExcel_jar/runApp.bat
@@ -1,1 +1,2 @@
 java -jar SynchExcel.jar
+timeout 1000
--- sql_queries/misc.sql
+++ sql_queries/misc.sql
@@ -6,8 +6,8 @@ JOIN smast on sstore.sfileId=smast.sfileid
 where  sFactCode='5201005079373';*/
 
 -- << Requested from Antoni : prints PDA transactions for a specific document(parastatiko). >>
-    --SELECT stdate,stDoc,sfactCode,sName,stquant,stTranskind,stLocation,stComment
-    --FROM STRN JOIN SMAST  on SMAST.sfileid=strn.sfileid and stDoc='4'
+   /* SELECT stdate,stDoc,sfactCode,sName,stquant,stTranskind,stLocation,stComment
+    FROM STRN JOIN SMAST  on SMAST.sfileid=strn.sfileid and stDoc='4'*/
 
 -- << Prints the the average cost price for bread. >>
     --SELECT sLastPrcPr FROM SMAST where sFactCode='43.13';
@@ -34,5 +34,61 @@ where sstore.sfileid='17764'; */
 /*insert into sstore
 (sFileId,SpaFileIdNo,sstRemain1,sstRemain2,sstApogr1,sstApogr2,sstWaiting1,sstWaiting2,sstOrdered1,sstOrdered2) 
 values (54,2,2,0,0,0,0,0,0,0);*/
+
+
+
+
+
+
+------------------------------------------
+
+
+/*insert into STRN
+( stDate,sFileId,stTransKind,stDoc,stLocation,stQuant,stQuant2,stPrice,stDiscount,stValue)
+Values (  '29-OCT-2019', 22390,	6,	4,	1,	 	4,	0,	0,	0,	0)*/
+
+/*insert into STRN
+( stDate,sFileId,stTransKind,stDoc,stLocation,stQuant,stQuant2,stPrice,stDiscount,stValue)
+Values (  '29-OCT-2019', 22390,	7,	4,	2,	 	4,	0,	0,	0,	0)*/
+
+--select * from SSTORE where SpaFileIdNo=2
+
+--	29/10/2019	22390	7	4	2	 	4	0	0	0	0
+
+
+--delete from strn where stdoc='4'
+--select * from strn where stdoc='4'
+
+/*insert into sstore
+(sFileId,SpaFileIdNo,sstRemain1,sstRemain2,sstApogr1,sstApogr2,sstWaiting1,sstWaiting2,sstOrdered1,sstOrdered2)
+values (22390,2,4,0,0,0,0,0,0,0)*/
+
+
+-----------------------------------------------------
+
+---< orders >----
+/*
+
+select sname,sfactCode,sLastPrcPr,SpaFileIdNo as apothiki,sstRemain1
+,sPerOutQ1,sPerOutQ2,sPerOutQ3,sPerOutQ4,sPerOutQ5,sPerOutQ6,sPerOutQ7
+sPerOutQ8,sPerOutQ9,sPerOutQ10,sPerOutQ11,sPerOutQ12
+ FROM BLINE2
+JOIN BMAST2 on bline2.pofileid=bmast2.pofileid
+JOIN smast on bline2.sfileid=smast.sfileid
+JOIN SSTORE on SSTORE.sfileid=SMAST.sfileid
+where poDocum='ΦΠP000002';
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 
